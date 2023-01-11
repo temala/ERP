1b4f142b5ba1fc018faef90acf37a7da97a7d27f
temala.ridha@gmail.com <gmailgmail,;12>
2023-01-11 17:45:38 +0100

    Add client relation to mission

---
+++ src/Infrastructure/Migrations/ApplicationDbContext2Migrations/20230111131705_missionClient.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Infrastructure.Migrations.ApplicationDbContext2Migrations
+{
+    /// <inheritdoc />
+    public partial class missionClient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClientId",
+                table: "Missions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Missions_ClientId",
+                table: "Missions",
+                column: "ClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Missions_Clients_ClientId",
+                table: "Missions",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Missions_Clients_ClientId",
+                table: "Missions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Missions_ClientId",
+                table: "Missions");
+
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Missions");
+        }
+    }
+}
