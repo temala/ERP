38c2bcbd83dd6d1985f2870eaebddf0f537cb2eb
temala.ridha@gmail.com <gmailgmail,;12>
2023-01-11 23:16:32 +0100

    CRA front add, details, list, details-empty

--- FrontUI/Angular-WebUI/client-app/src/app/mission/mission-list/mission-list.component.ts
+++ FrontUI/Angular-WebUI/client-app/src/app/mission/mission-list/mission-list.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit } from '@angular/core';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
